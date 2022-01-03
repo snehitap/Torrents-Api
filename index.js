8c5441fb8d0059492d08da3b725a1fcb28badf12
ovin <vincreator123@gmail.com>
2022-01-03 09:45:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const torrentGalaxy = require('./torrent/torrentGalaxy');
 const combo = require('./torrent/COMBO');
 const rarbg = require('./torrent/rarbg');
 const ettvCentral = require('./torrent/ettv');
+const zooqle = require('./torrent/zooqle');
+const kickAss = require('./torrent/kickAss');
+const bitSearch = require('./torrent/bitSearch');
+
 
 const app = express();
 
@@ -153,6 +157,62 @@ app.use('/api/:website/:query/:page?', (req, res, next) => {
             })
     }
 
+
+    if (website === 'zooqle') {
+        zooqle.zooqle(query, page)
+            .then((data) => {
+                if (data === null) {
+                    return res.json({
+                        error: 'Website is blocked change IP'
+                    })
+
+                } else if (data.length === 0) {
+                    return res.json({
+                        error: 'No search result available for query (' + query + ')'
+                    })
+                } else {
+                    return res.send(data);
+                }
+            })
+    }
+
+    if (website === 'kickass') {
+        kickAss(query, page)
+            .then((data) => {
+                if (data === null) {
+                    return res.json({
+                        error: 'Website is blocked change IP'
+                    })
+
+                } else if (data.length === 0) {
+                    return res.json({
+                        error: 'No search result available for query (' + query + ')'
+                    })
+                } else {
+                    return res.send(data);
+                }
+            })
+    }
+
+
+    if (website === 'bitsearch') {
+        bitSearch(query, page)
+            .then((data) => {
+                if (data === null) {
+                    return res.json({
+                        error: 'Website is blocked change IP'
+                    })
+
+                } else if (data.length === 0) {
+                    return res.json({
+                        error: 'No search result available for query (' + query + ')'
+                    })
+                } else {
+                    return res.send(data);
+                }
+            })
+    }
+
     if (website === 'nyaasi') {
         if (page > 14) {
             return res.json({
@@ -208,16 +268,16 @@ app.use('/api/:website/:query/:page?', (req, res, next) => {
             }
         })
 
-    } else if (website !== 'nyaasi' && website !== '1337x' && website !== 'yts' && website !== 'piratebay' && website !== 'torlock' && website !== 'eztv' && website !== 'tgx' && website !== 'all' && website !== "rarbg" && website !== 'ettv') {
+    } else if (website !== 'nyaasi' && website !== '1337x' && website !== 'yts' && website !== 'piratebay' && website !== 'torlock' && website !== 'eztv' && website !== 'tgx' && website !== 'all' && website !== "rarbg" && website !== 'ettv' && website !== 'zooqle' && website !== 'kickass' && website !== 'bitsearch') {
         return res.json({
-            error: 'please select 1337x | nyaasi | yts | Piratebay | torlock | eztv | TorrentGalaxy(tgx) | rarbg | all (to scrap from every site)'
+            error: 'please select 1337x | nyaasi | yts | Piratebay | torlock | eztv | TorrentGalaxy(tgx) | rarbg | zooqle | kickass | bitsearch | all (to scrap from every site)'
         })
     }
 
 });
 
 app.use('/', (req, res) => {
-    res.send('<h1>Welcome to 1337x, NyaaSi, YTS, PirateBay, Torlock, EzTvio , TorrentGalaxy , Rarbg and Ettv Central Unoffical API</h1>');
+    res.send('<h1>Welcome to 1337x, NyaaSi, YTS, PirateBay, Torlock, EzTvio, TorrentGalaxy, Rarbg, Zooqle, KickAss, Bitsearch and Ettv Central Unoffical API</h1>');
 });
 const PORT = process.env.PORT || 3001;
 console.log('Listening on PORT : ', PORT);
